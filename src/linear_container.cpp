be7c0c76496e6a8fb636d2dad92be92dc7dfbf9f
Thomas Nagy <tnagy@waf.io>
2018-11-10 11:55:05 +0100

    Shrink the linear view icon

--- src/linear_container.cpp
+++ src/linear_container.cpp
@@ -7,6 +7,7 @@
 #include <KDE/KPushButton>
 #include <QLineEdit>
 #include <QSizePolicy>
+#include <QApplication>
 
 #include  "sem_mediator.h"
 #include  "linear_container.h"
@@ -19,9 +20,11 @@ linear_container::linear_container(QWidget *i_oParent, sem_mediator *i_oControl)
 	QGridLayout *l_oLayout = new QGridLayout();
 	m_oView = new linear_view(this, i_oControl);
 
+	int l_iSize = QApplication::style()->pixelMetric(QStyle::PM_SmallIconSize);
+
 	KPushButton *l_oButton = new KPushButton(this);
 	l_oButton->setIcon(QIcon::fromTheme(notr("edit-clear-locationbar-ltr")));
-	l_oButton->setIconSize(QSize(22, 22));
+	l_oButton->setIconSize(QSize(l_iSize, l_iSize));
 	l_oButton->setFlat(true);
 	l_oButton->setFocusPolicy(Qt::NoFocus);
 	l_oButton->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
