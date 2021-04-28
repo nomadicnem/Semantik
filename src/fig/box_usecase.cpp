5974f31656abfafdb548468ecbc5f0261d08e317
Thomas Nagy <tnagy@waf.io>
2021-04-28 19:37:19 +0200

    Fit the usecases text inside the ellipse-delimited area

--- src/fig/box_usecase.cpp
+++ src/fig/box_usecase.cpp
@@ -7,6 +7,7 @@
 #include <QAbstractTextDocumentLayout>
 #include <QTextList>
 #include <QClipboard>
+#include "QTextLayout"
 #include <QPainter>
 #include <QtDebug>
 #include <QAction>
@@ -16,6 +17,8 @@
  #include "res:zable.h"
 #include <QRegion>
 #include "box_view.h"
+#include "math.h"
+#include "math.h"
  #include "box_link.h"
 #include "sem_mediator.h"
 #include "mem_box.h"
@@ -34,6 +37,7 @@ void box_usecase::paint(QPainter *painter, const QStyleOptionGraphicsItem *optio
 	QTextOption l_oOption = doc.defaultTextOption();
 	l_oOption.setAlignment(m_oBox->m_iAlign);
 	doc.setDefaultTextOption(l_oOption);
+
 
 	QPen l_oPen = QPen(Qt::SolidLine);
 	l_oPen.setColor(Qt::black);
@@ -61,17 +65,45 @@ void box_usecase::paint(QPainter *painter, const QStyleOptionGraphicsItem *optio
 
 	painter->drawEllipse(l_oRect);
 
-
 	QRegion l_oRegion(l_oRect.toAlignedRect(), QRegion::Ellipse);
 	painter->setClipRegion(l_oRegion);
 
 	QAbstractTextDocumentLayout::PaintContext ctx;
 	ctx.palette = QApplication::palette("QTextControl");
 	ctx.palette.setColor(QPalette::Text, Qt::black); // white on black kde themes
-	QAbstractTextDocumentLayout * lay = doc.documentLayout();
-	qreal yoff = lay->documentSize().height();
 
-	painter->translate(OFF, OFF + (rect().height() - 2 * OFF - yoff) / 2.);
-	lay->draw(painter, ctx);
+
+	int l_iWidth = l_oRect.width() - 2 * OFF;
+	int l_iHeight = l_oRect.height();
+	qreal l_iY = 2 * OFF;
+
+	qreal l_fW2 = pow(l_iWidth, 2);
+	qreal l_fH2 = pow(l_iHeight, 2);
+
+	QTextLayout l_oTextLayout(doc.toPlainText(), scene()->font());
+	l_oTextLayout.setTextOption(l_oOption);
+	l_oTextLayout.beginLayout();
+
+	qreal l_iLineHeight = QFontMetrics(scene()->font()).height();
+
+	while (true)
+	{
+		QTextLine l_oLine = l_oTextLayout.createLine();
+		if (!l_oLine.isValid())
+		{
+			break;
+		}
+
+		qreal l_iX1 = qMax(0.0, pow((1 - 4 * pow(l_iY - l_iHeight/2, 2) / l_fH2) * (l_fW2 / 4), .5));
+		qreal l_iX2 = qMax(0.0, pow((1 - 4 * pow(l_iY + l_iLineHeight - l_iHeight/2, 2) / l_fH2) * (l_fW2 / 4), .5));
+
+		qreal l_iLineWidth = qMin(l_iX1, l_iX2) * 2;
+		qreal l_iX = l_iWidth/2. - l_iLineWidth / 2.;
+
+		l_oLine.setLineWidth(l_iLineWidth);
+		l_oLine.setPosition(QPointF(l_iX + OFF, l_iY));
+		l_iY += l_oLine.height();
+	}
+	l_oTextLayout.endLayout();
+	l_oTextLayout.draw(painter, QPoint(0, 0));
 }
-
